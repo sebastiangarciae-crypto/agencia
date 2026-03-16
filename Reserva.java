128e3e86d6bbbbe5307dc4063e78f4608b314078
mariajperezgo-oss <mariaj.perezgo@autonoma.edu.co>
2026-03-16 16:17:38 -0500

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,30 @@
+
+
 public class Reserva{
 
+    protected Cliente cliente;
+    protected Hotel hotel;
+    protected Vuelo vuelo;
+    protected String fechaReserva;
 
-  
+    public Reserva(Cliente cliente, Hotel hotel, Vuelo vuelo, String fechaReserva){
+        this.cliente = cliente;
+        this.hotel = hotel;
+        this.vuelo = vuelo;
+        this.fechaReserva = fechaReserva;
+    }
+
+    public Cliente getCliente(){return cliente;}
+    public Hotel getHotel(){return hotel;}
+    public Vuelo getVuelo(){return vuelo;}
+    public String getFechaReserva(){return fechaReserva;}
+
+    public void mostrarInfo(){
+        System.out.println("=== Reserva ===");
+        System.out.println("Fecha de reserva: " + fechaReserva);
+        cliente.mostrarInfo();
+        hotel.mostrarInfo();
+        vuelo.mostrarInfo();
+    }
 }
+
