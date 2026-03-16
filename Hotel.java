a085a5b6c8d5dec639a96184823773b5a99f9668
mariajperezgo-oss <mariaj.perezgo@autonoma.edu.co>
2026-03-16 16:16:25 -0500

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,5 +1,27 @@
+
 public class Hotel{
 
+    protected String nombre;
+    protected String ciudad;
+    protected int estrellas;
+    protected double precioPorNoche;
 
+    public Hotel(String nombre, String ciudad, int estrellas, double precioPorNoche){
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.estrellas = estrellas;
+        this.precioPorNoche = precioPorNoche;
+    }
 
+    public String getNombre(){return nombre;}
+    public String getCiudad(){return ciudad;}
+    public int getEstrellas(){return estrellas;}
+    public double getPrecioPorNoche(){return precioPorNoche;}
+
+    public void mostrarInfo(){
+        System.out.println("Hotel: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Estrellas: " + estrellas);
+        System.out.println("Precio por noche: " + precioPorNoche);
+    }
 }
