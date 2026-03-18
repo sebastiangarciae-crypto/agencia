8ae960caba5dff5b96c8109fb02dc4ac1c08eb15
JuanMignio <dylanmontes66@gmail.com>
2026-03-17 19:11:23 -0500

    En la clase Hotel agregue unos atributos que serian las caracteristicas del hotel como cuantas habitaciones tiene, su nombre, etc y agregue unos metodos que  llaman esos attributos y los imprime

--- Hotel.java
+++ Hotel.java
@@ -1,5 +1,22 @@
 public class Hotel{
+    int idHotel;
+    String ubicacion;
+    int capacidadHotel = 30;
+    String nombreHotel;
 
+    void idHotel (){
+        System.out.println("ID Hotel:" + idHotel);
+    }
 
+    void ubicacion (){
+        System.out.println("ubicacion:" + ubicacion);
+    }
 
+    void capacidadHotel(){
+        System.out.println("Capacidad:" + capacidadHotel + " huespedes");
+    }
+
+    void nombreHotel (){
+        System.out.println("nombre:" + nombreHotel);
+    }
 }
