08c88ca89be2ac5c55324c4309e338eb17e8d97b
santi-1805 <furqueosorio2@gmail.com>
2026-03-24 19:29:59 -0500

    Añadi, calcular el valor total de la reserva

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,23 @@
-public class Reserva{
+public class Reserva {
+    private String nombreCliente;
+    private int dias;
+    private double precioPorNoche;
 
+    public Reserva(String nombreCliente, int dias, double precioPorNoche) {
+        this.nombreCliente = nombreCliente;
+        this.dias = dias;
+        this.precioPorNoche = precioPorNoche;
+    }
 
-  
+    public double calcularCostoTotal() {
+        return dias * precioPorNoche;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("===== DETALLES DE LA RESERVA =====");
+        System.out.println("Cliente: " + nombreCliente);
+        System.out.println("Días de estadía: " + dias);
+        System.out.println("Precio por noche: $" + precioPorNoche);
+        System.out.println("Total a pagar: $" + calcularCostoTotal());
+    }
 }
